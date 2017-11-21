76f2625f61b70a415fb86d6a4f4c623d39736085
Susmita Horrow <susmita@Susmita-MacBook-Pro.local>
2017-11-21 08:28:44 +0530

    test(formField): add FormFieldTests class

---
+++ SwiftIndiaTDD/SwiftIndiaTDDTests/FormFieldTests.swift
@@ -0,0 +1,22 @@
+//
+//  FormFieldTests.swift
+//  SwiftIndiaTDDTests
+//
+//  Created by Susmita Horrow on 21/11/17.
+//  Copyright © 2017 hsusmita. All rights reserved.
+//
+
+import XCTest
+
+class FormFieldTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+}
