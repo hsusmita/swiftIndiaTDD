c67e9f4934d4ab7cb4bc9ba81f0c05a72b84adb1
Susmita Horrow <susmita@Susmita-MacBook-Pro.local>
2017-11-22 07:57:32 +0530

    test(formField): add init method in FormField class(green step)

---
+++ SwiftIndiaTDD/SwiftIndiaTDD/FormField.swift
@@ -0,0 +1,13 @@
+//
+//  FormField.swift
+//  SwiftIndiaTDD
+//
+//  Created by Susmita Horrow on 22/11/17.
+//  Copyright © 2017 hsusmita. All rights reserved.
+//
+
+import Foundation
+
+class FormField {
+	init(value: String, validator: @escaping (String) -> Bool ) {}
+}
