c8a44ebf6a3d021e04ff5ac445504f88585e27fc
Susmita Horrow <susmita@Susmita-MacBook-Pro.local>
2017-11-27 16:34:56 +0530

    test(networkManager): test signup method(red step)

--- SwiftIndiaTDD/SwiftIndiaTDDTests/NetworkManagerTests.swift
+++ SwiftIndiaTDD/SwiftIndiaTDDTests/NetworkManagerTests.swift
@@ -16,22 +16,24 @@ class NetworkManagerTests: XCTestCase {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
+
+	func test_signup() {
+		let expectation = XCTestExpectation(description: "Signup")
+
+		sut.signup(username: "hello", password: "helloworld", completion: { result in
+			switch result {
+			case .success(let success):
+				XCTAssert(true, "SignupSuccessful")
+				expectation.fulfill()
+			case .error(let error):
+				XCTAssert(false, "SignupError")
+			}
+		})
+		wait(for: [expectation], timeout: 30.0)
+	}
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
