f427326ce565c36a8471b7b5c7699113eb51ecfb
Susmita Horrow <susmita@Susmita-MacBook-Pro.local>
2017-11-27 15:10:03 +0530

    test(signupModel): start tests for SignupViewModel(red step)

--- SwiftIndiaTDD/SwiftIndiaTDD/SignupViewController.swift
+++ SwiftIndiaTDD/SwiftIndiaTDD/SignupViewController.swift
@@ -15,15 +15,12 @@ class SignupViewController: UIViewController {
 
 	override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
     /*
     // MARK: - Navigation
