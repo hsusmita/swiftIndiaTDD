5a0e81323a9ac41fa6e90443b7ed3ca951c8e989
Susmita Horrow <susmita@Susmita-MacBook-Pro.local>
2017-11-27 16:29:36 +0530

    test(networkManager): add networkManager class(green step)

---
+++ SwiftIndiaTDD/SwiftIndiaTDD/NetworkManager.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkManager.swift
+//  SwiftIndiaTDD
+//
+//  Created by Susmita Horrow on 27/11/17.
+//  Copyright © 2017 hsusmita. All rights reserved.
+//
+
+import Foundation
+
+class NetworkManager {
+	
+}
