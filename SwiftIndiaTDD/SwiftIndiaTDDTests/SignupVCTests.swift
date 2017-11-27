95962362f1df5e55dcd00a62019f543ca923d730
Susmita Horrow <susmita@Susmita-MacBook-Pro.local>
2017-11-27 14:57:40 +0530

    test(signupVC): add test for textfields not nil(red step)

--- SwiftIndiaTDD/SwiftIndiaTDDTests/SignupVCTests.swift
+++ SwiftIndiaTDD/SwiftIndiaTDDTests/SignupVCTests.swift
@@ -14,24 +14,19 @@ class SignupVCTests: XCTestCase {
 
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+		let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+		sut = storyboard.instantiateViewController(withIdentifier: "SingupVC") as! SignupViewController
+		_ = sut.view
     }
+
+	func test_initialSetup() {
+		XCTAssertNotNil(sut.usernameTextField)
+		XCTAssertNotNil(sut.passwordTextField)
+		XCTAssertNotNil(sut.confirmPasswordTextField)
+	}
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
