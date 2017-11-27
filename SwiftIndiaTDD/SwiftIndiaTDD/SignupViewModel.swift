5badb43cd044d932e6667e435bc0102a2a9cba74
Susmita Horrow <susmita@Susmita-MacBook-Pro.local>
2017-11-27 15:11:16 +0530

    test(signupModel): add SignupViewModel class(green step)

---
+++ SwiftIndiaTDD/SwiftIndiaTDD/SignupViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  SignupViewModel.swift
+//  SwiftIndiaTDD
+//
+//  Created by Susmita Horrow on 27/11/17.
+//  Copyright © 2017 hsusmita. All rights reserved.
+//
+
+import Foundation
+
+class SignupViewModel {
+
+}
